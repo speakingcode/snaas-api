6e0d90fc57ebf28b61a6f379694de8fb1663b8d0
speakingcode <dlissner@gmail.com>
2018-06-28 13:46:53 -0500

    Include album format in json resp

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -11,7 +11,7 @@ module Api::V1
       respond_to do |format|
         format.json {
           render :json => albums.to_json(
-            :include => [:certifications, :peak_chart_positions => {:include => :album_chart }]
+            :include => [:certifications, :album_formats, :peak_chart_positions => {:include => :album_chart }]
           )
         }
       end
